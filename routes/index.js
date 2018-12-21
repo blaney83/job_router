069174e7bc501bfc22c227f7a09f88f838e0d2fc
Blaney83 <professionallaney@gmail.com>
2018-12-20 23:02:11 -0700

    Fixing build routes.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.use("/v1", v1Routes);
 
 // Program in a fall back route for missed calls
 router.use(function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../app/build/index.html"));
 });
 
 module.exports = router;
