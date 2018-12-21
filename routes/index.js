a466ef302e21f8fcdf4a93c8c686754c54f2a1cd
Blaney83 <professionallaney@gmail.com>
2018-12-20 22:48:53 -0700

    Fixing build routes.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const v1Routes = require("./v1")
 
 router.use("/v1", v1Routes);
 
-Program in a fall back route for missed calls
+// Program in a fall back route for missed calls
 router.use(function (req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
